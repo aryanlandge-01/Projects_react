5bd486367dac16cd0e75d389ab79e1a4f1327365
aryanlandge-01 <landgearyan0@gmail.com>
2023-12-14 19:37:57 +0530

    appwrite configuration

---
+++ 12Blog-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import conf from './Conf/conf'
+
+function App() {
+  
+
+
+  return (
+    <>
+       <h1 className='text-cyan-600 font-extrabold text-center text-7xl'>App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
